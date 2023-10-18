6c1b4b419a517872a2da951bffd1d456599cce88
Mert Polat <mert.dev@icloud.com>
2023-10-18 15:22:39 +0300

    Initial Commit

---
+++ Netflix/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Netflix
+//
+//  Created by mert polat on 18.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
