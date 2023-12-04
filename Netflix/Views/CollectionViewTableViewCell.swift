033422bf713b92ffb84e5b32ca4ead4ab015ef73
Mert Polat <mert.dev@icloud.com>
2023-12-04 17:07:43 +0300

    preview screen added

--- Netflix/Views/CollectionViewTableViewCell.swift
+++ Netflix/Views/CollectionViewTableViewCell.swift
@@ -7,9 +7,16 @@
 
 import UIKit
 
+
+protocol CollectionViewTableCellDelegate: AnyObject {
+    func CollectionViewTableCellDidTapCell(_ cell:CollectionViewTableViewCell, viewModel: TitlePreviewViewModel)
+}
+
 class CollectionViewTableViewCell: UITableViewCell {
 
    static let identifier = "CollectionViewTableViewCell"
+    
+    weak var delegate: CollectionViewTableCellDelegate?
     
     private var titles: [Title] = [Title]()
     
@@ -63,5 +70,33 @@ extension CollectionViewTableViewCell: UICollectionViewDelegate,UICollectionView
         }
         cell.configure(with: model)
         return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        collectionView.deselectItem(at: indexPath, animated: true)
+        
+        let title = titles[indexPath.row]
+        guard let titleName = title.original_title ?? title.original_name else {
+            return
+        }
+        
+        ApiCaller.shared.getMovie(with: titleName + "trailer") { [weak self] result in
+            switch result{
+            case .success(let videoElement):
+                
+                var title = self?.titles[indexPath.row]
+                guard let titleOverview = title?.overview else {
+                    return
+                }
+                        
+                guard let strongSelf =  self else {
+                    return
+                }
+                let viewModel = TitlePreviewViewModel(title: titleName, youtubeView: videoElement, titleOverview: titleOverview)
+                self?.delegate?.CollectionViewTableCellDidTapCell(strongSelf, viewModel: viewModel )
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        }
     }
 }
